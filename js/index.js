99fa846b87a14502443262d5ad37d62201fa0fb0
justin <jberger@melco.com>
2015-08-02 20:02:50 -0600

    Gist support now included

--- js/index.js
+++ js/index.js
@@ -1,16 +1,28 @@
 var $ = require('jquery');
 var CodeMirror = require('CodeMirror');
-require('CodeMirror/mode/javascript/javascript');
 var esprima = require('esprima'); 
 var js2glsl = require('js2glsl');
-var nodeUtils = require('js2glsl/libs/nodeUtils');
+var nodeUtils = require('js2glsl/libs/nodeUtils'),
+    CoffeeScript = require('coffee-script')
+    
+require('CodeMirror/mode/javascript/javascript');
+require('CodeMirror/mode/clike/clike');
 
 var canvas = $("canvas")[0];
 
-var editor = CodeMirror.fromTextArea($("textarea")[0], {
+var editor = CodeMirror.fromTextArea($("#jssrc")[0], {
     lineNumbers: true,
     mode: "text/javascript"
 });
+
+
+var viewer = CodeMirror.fromTextArea($("#glslsrc")[0], {
+    lineNumbers: true,
+    mode: "x-shader/x-fragment",
+    readOnly:true
+});
+
+$(viewer.getWrapperElement()).toggle();
 
 var gl = canvas.getContext("webgl"); 
 var createShader = require('gl-shader');
@@ -31,17 +43,88 @@ function VertexPosition() {
     return [ attributes.pt[0], attributes.pt[1] ]; 
 };
 
-function setupShader() {
+var lastSaveId = window.location.hash.replace("#","");
+
+if(lastSaveId) {
+    $.ajax( "https://api.github.com/gists/" + lastSaveId, {
+        method: "GET"
+    }).done(function(data) {
+        lastSaveId = data.id;
+        lastSourceSave = (data.files["source"] && data.files["source"].content) || "";     
+        editor.setValue(lastSourceSave);
+        setupShader(); 
+    });
+}
+
+var lastSourceSave = "";
+function saveSource() {
+    var src = editor.doc.getValue();
+    setupShader();
+    if(lastSourceSave != src) {            
+            $.ajax( "https://api.github.com/gists", {
+                method: "POST",
+                dataType: 'json',
+                data: JSON.stringify({                  
+                  "public": true,
+                  "files": {
+                    "source": {
+                      "content": src
+                    },
+                    'output': {
+                      'content': viewer.doc.getValue()
+                    }
+                  }
+                })                
+            }).done(function(data) {
+                window.location.hash = '#' + data.id;
+                lastSaveId = data.id;
+                lastSourceSave = src; 
+            });        
+    }
+}
+
+function setupShader() {    
+    try {
     
-    var jsSource = editor.doc.getValue();
+    var langSource = editor.doc.getValue();
+    var langIdLine = langSource.split('\n')[0]; 
+    
+    var langRegex = /-\*-(.*)-\*-/; 
+    var langMatch = langRegex.exec(langIdLine); 
+    var jsSource; 
+    
+    if(langMatch && langMatch[1] && langMatch[1].toLowerCase) {
+        switch(langMatch[1].trim().toLowerCase()){
+            case '':
+            case 'js':
+            case 'javascript':
+                jsSource = langSource;
+                break;
+            case 'cs':
+            case 'coffeescript':
+                jsSource = CoffeeScript.compile(langSource);
+                break;
+            default:
+                throw new Error("Could not identify language " + langMatch[1].trim().toLowerCase()); 
+        }
+    } else {
+        jsSource = langSource;
+    }
+    viewer.setValue( 'Error creating glsl from: \r\n\r\n' + jsSource);
+    
     var ast = esprima.parse(jsSource);
     
     if(nodeUtils.getFunctionByName(ast, "VertexPosition") === undefined) {
         ast.body.push( esprima.parse(VertexPosition.toString()).body[0] ); 
     }
-    
-    try {
+        
     var shaderSource = js2glsl(ast); 
+      viewer.setValue( ["/*** Vertex shader ***/",
+                        shaderSource.vertex,
+                        "/*** Fragment shader ***/",
+                        shaderSource.fragment,
+                        "/*** Lang source ***/", 
+                        jsSource ].join("\n") );
     
         var shader = createShader(gl,
             shaderSource.vertex,
@@ -54,17 +137,27 @@ function setupShader() {
       shader.attributes.pt.pointer();      
       $("#errors").hide();
       $("canvas").show();
-    } catch(e) {
-        var whereAt = "<uncompilable>";
-        if(shaderSource && shaderSource.vertex)
-            whereAt = shaderSource.vertex + "\r\n----\r\n" + shaderSource.fragment;
-        $("#errors").text( e.toString() + " in: \r\n" + whereAt);
+      
+    } catch(e) {        
+        $("#errors").text( e.toString());
         $("#errors").show();
         $("canvas").hide();
     }
 }
 
+$("#save").on('click', saveSource); 
 $("#go").on('click', setupShader); 
+
+function switchViews() {
+        $(".CodeMirror").toggle();
+        viewer.refresh();
+        editor.refresh();
+}
+$("#toggle").on('click', switchViews); 
+$("#report").on('click', function() {
+    saveSource();
+    window.open('https://github.com/jdavidberger/js2glsl/issues', '_blank');
+});
 setupShader();
 var start = Date.now();
 function render() {
